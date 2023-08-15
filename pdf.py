7004cd5d52e85d190ca7464eb6c8436d31507267
Eduardo-byte <eduardo7.brito@gmail.com>
2023-08-15 19:30:07 +0100

    add delete page function

--- pdf.py
+++ pdf.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import fitz
 
 # with open('pdf_files/dummy.pdf' , 'rb') as file:
 #     reader = PyPDF2.PdfReader(file)
@@ -11,13 +12,32 @@ import PyPDF2
 #     with open('pdf_files/tilt.pdf'  , 'wb') as new_file:
 #         writer.write(new_file)      
 
-pdf_list = ['pdf_files/dummy.pdf' , 'pdf_files/twopage.pdf' , 'pdf_files/tilt.pdf']
+# pdf_list = ['pdf_files/dummy.pdf' , 'pdf_files/twopage.pdf' , 'pdf_files/tilt.pdf']
 
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('pdf_files/super.pdf')
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('pdf_files/super.pdf')
         
-pdf_combiner(pdf_list)
+# pdf_combiner(pdf_list)
+
+  
+# Path of the PDF file
+input_file = r"pdf_files/Eduardo_Brito_CV_Developer.pdf"
+  
+# Path for the output PDF file
+output_file = r"pdf_files/modified_test.pdf"
+  
+# Opening the PDF file and creating a handle for it
+file_handle = fitz.open(input_file)
+  
+# The page no. denoted by the variable will be deleted
+page = 8
+  
+# Passing the variable as an argument
+file_handle.delete_page(page)
+  
+# Saving the file
+file_handle.save(output_file)
